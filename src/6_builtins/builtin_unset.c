760eadb2a6f34722fd5eb563b7bdb324e26a58f0
Patricia Bastida 🎯 <patriciabastida@gmail.com>
2024-02-08 13:43:55 +0100

    avances en builtin unset

--- src/6_builtins/builtin_unset.c
+++ src/6_builtins/builtin_unset.c
@@ -6,33 +6,35 @@
 /*   By: pabastid <pabastid@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:17:07 by pabastid          #+#    #+#             */
-/*   Updated: 2024/02/08 12:26:02 by pabastid         ###   ########.fr       */
+/*   Updated: 2024/02/08 13:43:19 by pabastid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // comprobar si unset tira error cuando usamos caracteres especiales (@) -> hay que crear una funcion que comprueba que no hay caracteres especiales >> que usaremos en export tambien
-int builtin_unset(**env_char, [array de args]) // recibe el **env_char que resulta de execve.c->converting la lista env y el array de args que van detras <UNSET hola holaaaa ciao pescao>
+int builtin_unset(**env_char, t_cmd[array de args]) // recibe el **env_char que resulta de execve.c->converting la lista env y el array de args que van detras <UNSET hola holaaaa ciao pescao>
 {
 	int i;
 
-	i =
-		if (![array[1]]) // si ponemos solo unset no hace nada
+	i = 1;
+	if (!t_cmd ![array[1]]) // si ponemos solo unset no hace nada
 		return (0);
 	else
 	{
 		while (args[i]) // mientras haya argumentos que borrar;
 		{
-			1 - comprobamos si hay carcateres especiales para que tire error
-			// t_env nodo = con la funcion ft_getenv(que le daremos el arg que esta analizando comprueba si esta en la lista * *env y si la encuentra devolvera el nodo)
+			puntero del nodo anterior = get_envnode(arg[i]);
 			{
-				if (!el nodo)
-					->continue;
-				if (existe el nodo)
-					->lo borra;
-				Al borrar hay que comprobar : 1 - Si es el primer nodo de la lista->hay que ponerlo en NULL porque al borrarlo se queda vacio;
-				2 - Si no es el primer nodo->hay que cambiar el prev y el next, y el prev del prev y del next que acabara en null.
+				if (get_envnode no encuetra nada)
+					return (0);
+				if (existe puntero del nodo anterior = get_envnode(arg[i]))
+					1 - si el resultado de get_envnode es el primer nodo->swap con free del primero
+							tmp = master->env->next;
+				free(master->env) ojo que free de todo
+					master->env = tmp;
+
+				2 - si tmp->next != NULL->get_envnode tiene que devolver el anterior(tmp) y hacer free de(tmp->next);
 			}
 		}
 		return (0);
